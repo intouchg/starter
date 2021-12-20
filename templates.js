09861951e776bdf3cf4af2af1284164e120c7202
Cody Persinger <codypersinger@gmail.com>
2021-12-20 16:04:49 -0600

    Prettify CLI

--- templates.js
+++ templates.js
@@ -9,11 +9,17 @@ const path = require('path')
 const chalk = require('chalk')
 
 const packageKeyBlacklist = [
-	'name', 'version', 'private', 'repository', 'author', 'license',
+	'name',
+	'version',
+	'private',
+	'repository',
+	'author',
+	'license',
 ]
 
 let validationErrors = ''
-const addValidationError = (error) => validationErrors += `${validationErrors ? '\n' : ''}${error}`
+const addValidationError = (error) =>
+	(validationErrors += `${validationErrors ? '\n' : ''}${error}`)
 
 const validateTemplate = (templateName, templatePath) => {
 	const packageJsonPath = path.join(templatePath, 'package.json')
@@ -21,22 +27,23 @@ const validateTemplate = (templateName, templatePath) => {
 	const yarnLockPath = path.join(templatePath, 'yarn.lock')
 
 	let errorList = ''
-	const addErrorListItem = (error) => errorList += `${errorList ? '\n' : ''}  ${error}`
+	const addErrorListItem = (error) =>
+		(errorList += `${errorList ? '\n' : ''}  ${error}`)
 
 	if (!fs.existsSync(packageJsonPath))
 		addErrorListItem(`* package.json must exist`)
 
-	if (!fs.existsSync(readmePath))
-		addErrorListItem(`* readme.md must exist`)
+	if (!fs.existsSync(readmePath)) addErrorListItem(`* readme.md must exist`)
 
-	if (!fs.existsSync(yarnLockPath))
-		addErrorListItem(`* yarn.lock must exist`)
+	if (!fs.existsSync(yarnLockPath)) addErrorListItem(`* yarn.lock must exist`)
 
 	const packageJson = require(packageJsonPath)
 	const readme = fs.readFileSync(readmePath).toString()
 
 	if (!packageJson.description)
-		addErrorListItem('* package.json must contain a "description" property that describes your template')
+		addErrorListItem(
+			'* package.json must contain a "description" property that describes your template'
+		)
 
 	packageKeyBlacklist.forEach((key) => {
 		if (packageJson[key] === undefined) return
@@ -47,17 +54,24 @@ const validateTemplate = (templateName, templatePath) => {
 		addErrorListItem('* readme.md must contain "[PROJECT NAME]"')
 
 	if (errorList)
-		addValidationError(`\nERROR: Invalid template "${templateName}" at path ${templatePath}\n${errorList}`)
+		addValidationError(
+			`\nERROR: Invalid template "${templateName}" at path ${templatePath}\n${errorList}`
+		)
 
 	return packageJson
 }
 
-console.log('INFO: Validating @intouchg/starter templates and generating metadata')
-console.log('INFO: Read more about template requirements here: https://github.com/intouchg/starter#template-requirements')
+console.log(
+	'INFO: Validating @intouchg/starter templates and generating metadata'
+)
+console.log(
+	'INFO: Read more about template requirements here: https://github.com/intouchg/starter#template-requirements'
+)
 
 const templatesPath = path.join(__dirname, 'templates')
 
-const templates = fs.readdirSync(templatesPath, { withFileTypes: true })
+const templates = fs
+	.readdirSync(templatesPath, { withFileTypes: true })
 	.filter((dirent) => dirent.isDirectory())
 	.map(({ name }) => {
 		const pkg = validateTemplate(name, path.join(templatesPath, name))
@@ -65,12 +79,20 @@ const templates = fs.readdirSync(templatesPath, { withFileTypes: true })
 	})
 
 if (validationErrors) {
-	console.log(chalk.red(`ERROR: Failed to generate templates.json\n${validationErrors}\n`))
+	console.log(
+		chalk.red(
+			`ERROR: Failed to generate templates.json\n${validationErrors}\n`
+		)
+	)
 	process.exit(1)
 }
 
-const defaultIndex = templates.findIndex((template) => template.name === 'default')
-const orderedTemplates = templates.filter((template, index) => index !== defaultIndex)
+const defaultIndex = templates.findIndex(
+	(template) => template.name === 'default'
+)
+const orderedTemplates = templates.filter(
+	(template, index) => index !== defaultIndex
+)
 orderedTemplates.unshift(templates[defaultIndex])
 
 const jsonPath = path.join(__dirname, 'lib', 'templates.json')
